5523645d440307938c9a2feaa8231770ca62be72
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-09-07 13:02:19 +0200

    click increments page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-
+import { useState } from 'react'
 import './App.css'
 
 
 export default function App() {
+  const [page, setPage] = useState<number>(0);
   return (
     <>
       <div className="container">
-          <div className="arrow left">Left Arrow</div>
-          <div className="content">Pair Programming Survival Kit</div>
-          <div className="arrow right">Right Arrow</div>
+          <div className="arrow">Left Arrow</div>
+          <div className="content">Page {page}</div>
+          <div className="arrow" onClick={(e) => setPage(page+1)}>
+          Right Arrow
+          </div>
       </div>
     </>
   )
