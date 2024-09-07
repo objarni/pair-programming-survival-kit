447f53b685e8cca0e43ef7c2336d858c4fb4b7a5
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-09-07 12:57:15 +0200

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <>
       <div className="container">
           <div className="arrow left">Left Arrow</div>
-          <div className="middle">Pair Programming Survival Kit</div>
+          <div className="content">Pair Programming Survival Kit</div>
           <div className="arrow right">Right Arrow</div>
       </div>
     </>
