41a0966bdcf82ecdeb6f6f8767fe5aa54ba9060b
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-09-07 13:16:53 +0200

    cannot go past first page

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,22 @@ export default function App() {
   return (
     <>
       <div className="container">
-          <div className="arrow" onClick={() => setPage(page-1)}>
-            &lt;
-          </div>
+          {page > 0 && (
+            <div className="arrow" onClick={() => setPage(page-1)}>
+              Back
+            </div>
+          )}
+          { page === 0 && (
+            <div className="arrow">
+              &nbsp;
+            </div>
+          )}
           <div className="content">
             Page {page}
             {texts[page]}
           </div>
           <div className="arrow" onClick={() => setPage(page+1)}>
-            &gt;
+            Next
           </div>
       </div>
     </>
