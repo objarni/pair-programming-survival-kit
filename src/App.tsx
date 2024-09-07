24bcae90479e8feb740c8dbd91142195331943c1
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-09-07 13:09:36 +0200

    texts array

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 
+const texts: string[] = [
+  "hello",
+  "world"
+]
+
 export default function App() {
   const [page, setPage] = useState<number>(0);
   return (
@@ -10,7 +15,10 @@ export default function App() {
           <div className="arrow" onClick={() => setPage(page-1)}>
             &lt;
           </div>
-          <div className="content">Page {page}</div>
+          <div className="content">
+            Page {page}
+            {texts[page]}
+          </div>
           <div className="arrow" onClick={() => setPage(page+1)}>
             &gt;
           </div>
